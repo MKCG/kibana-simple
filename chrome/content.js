39954939a0adab872ed26288c33d80807effacc2
Kévin Masseix <kmasseix@fashionnetwork.com>
2019-05-14 10:04:48 +0200

    Always expand navbar

--- chrome/content.js
+++ chrome/content.js
@@ -1,4 +1,4 @@
-if (window.location.href.indexOf('/app/kibana') !== -1) {
+if (window.location.href.indexOf('/app/kibana') !== -1 || window.location.href.indexOf('/app/monitoring') !== -1) {
     window.addEventListener("load", function() {
         let hideUnwantedMenu = function(hideUnwantedMenu) {
             setTimeout(function(hideUnwantedMenu) {
@@ -9,10 +9,32 @@ if (window.location.href.indexOf('/app/kibana') !== -1) {
                         .filter(i => ['Maps', 'Machine Learning', 'Infrastructure', 'APM', 'Uptime', 'Graph', 'Logs', 'Canvas'].indexOf(i.getAttribute('title')) !== -1)
                         .forEach(i => i.parentNode.parentNode.parentNode.classList.add('hide'))
                     ;
+
+                    [...document.getElementsByClassName('euiNavDrawer')]
+                        .forEach(function(i) {
+                            if (i.classList.contains('euiNavDrawer-isCollapsed') === true) {
+                                i.classList.remove('euiNavDrawer-isCollapsed');
+                            }
+
+                            if (i.classList.contains('euiNavDrawer-isExpanded') === false) {
+                                i.classList.add('euiNavDrawer-isExpanded');
+                            }
+                        });
+
+                    [...document.getElementsByClassName('app-wrapper')]
+                        .forEach(i => i.style.left = '240px')
+                    ;
+
+                    [...document.getElementsByClassName('euiNavDrawer__expandButton')]
+                        .forEach(i => i.classList.add('hide'))
+                    ;
                 }
             }, 250, hideUnwantedMenu);
         };
 
-        hideUnwantedMenu(hideUnwantedMenu);
+        // Kibana seems to be used
+        if (document.getElementsByClassName('kibanaWelcomeView').length > 0) {
+            hideUnwantedMenu(hideUnwantedMenu);
+        }
     }, true);
 }
